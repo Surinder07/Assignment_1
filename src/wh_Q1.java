83469f4a5339b23e11c4bb51a92105672c641f8b
Surinder Singh <Surinder0322@gmail.com>
2023-02-09 15:09:45 -0500

    Update wh_Q1.java
    Change the class name to capital 'W'

--- src/wh_Q1.java
+++ src/wh_Q1.java
@@ -1,7 +1,7 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-public class wh_Q1 {
+public class Wh_Q1 {
     public static void main(String[] args) {
         System.out.printf("Enter the number: ");
         Scanner sc=new Scanner(System.in);
